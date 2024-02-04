c43d4b159f61ca31f1b7d53d2cce29e9b33b7354
enaa99 <wprkfdlsgk@gmail.com>
2024-02-04 18:05:11 +0900

    initial commit

---
+++ membership_service/src/main/java/com/membership/MemberShipApplication.java
@@ -0,0 +1,11 @@
+package com.membership;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberShipApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MemberShipApplication.class, args);
+    }
+}
