74f3e1f76a6092d9f123e1a33cb7e60f9d90b964
enaa99 <wprkfdlsgk@gmail.com>
2024-02-04 21:45:23 +0900

    feat: add Membership domain and change controller name to RegisterMemberShopController

--- membership_service/src/main/java/com/membership/domain/Membership.java
+++ membership_service/src/main/java/com/membership/domain/Membership.java
@@ -3,6 +3,7 @@ package com.membership.domain;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Value;
 
 @Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
@@ -13,4 +14,67 @@ public class Membership {
     private final String address;
     private final boolean isValid;
     private final boolean isCorp;
+
+    public static Membership generateMembership(
+        MembershipId membershipId,
+        MembershipName membershipName,
+        MembershipEmail membershipEmail,
+        MembershipAddress membershipAddress,
+        MembershipIsValid membershipIsValid,
+        MembershipIsCorp membershipIsCorp
+    ){
+        return new Membership(
+                membershipId.valuId,
+                membershipName.valueName,
+                membershipEmail.valueEmail,
+                membershipAddress.valueEmail,
+                membershipIsValid.valueValid,
+                membershipIsCorp.valueIsCorp
+        );}
+
+
+    @Value
+    public static class MembershipId {
+        String valuId;
+        public MembershipId(String value) {
+            this.valuId = value;
+        }
+    }
+    @Value
+    public static class MembershipName {
+        String valueName;
+        public MembershipName(String value) {
+            this.valueName = value;
+        }
+    }
+    @Value
+    public static class MembershipEmail {
+        String valueEmail;
+        public MembershipEmail(String value) {
+            this.valueEmail = value;
+        }
+    }
+    @Value
+    public static class MembershipAddress {
+        String valueEmail;
+        public MembershipAddress(String value) {
+            this.valueEmail = value;
+        }
+    }
+
+    @Value
+    public static class MembershipIsValid {
+        boolean valueValid;
+        public MembershipIsValid(boolean value) {
+            this.valueValid = value;
+        }
+    }
+
+    @Value
+    public static class MembershipIsCorp {
+        boolean valueIsCorp;
+        public MembershipIsCorp(boolean value) {
+            this.valueIsCorp = value;
+        }
+    }
 }
