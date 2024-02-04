f82f28f4ef5893213a2e53d675256a4a1eb0a00d
enaa99 <wprkfdlsgk@gmail.com>
2024-02-04 18:12:08 +0900

    feat: add membership domain to use private

---
+++ membership_service/src/main/java/com/membership/domain/Membership.java
@@ -0,0 +1,16 @@
+package com.membership.domain;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Membership {
+    private final String membershipId;
+    private final String name;
+    private final String email;
+    private final String address;
+    private final boolean isValid;
+    private final boolean isCorp;
+}
