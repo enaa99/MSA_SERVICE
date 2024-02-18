e87dd020d13bbb1772b2d7e5150eeb3fc3c8da21
enaa99 <wprkfdlsgk@gmail.com>
2024-02-18 23:05:28 +0900

    feat: add ModifyMembershipCommand static import

--- membership_service/src/test/java/com/membership/application/port/in/ModifyMembershipCommandTest.java
+++ membership_service/src/test/java/com/membership/application/port/in/ModifyMembershipCommandTest.java
@@ -1,21 +1,23 @@
 package com.membership.application.port.in;
 
 import jakarta.validation.ValidationException;
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static com.membership.application.port.in.ModifyMembershipCommand.*;
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 @RunWith(SpringRunner.class)
 class ModifyMembershipCommandTest {
     @Test
+    @DisplayName("ModifyMembershipCommand 실패 테스트")
     void validateFailSelf() {
-        Assertions.assertThrows(ValidationException.class, () -> {
-            ModifyMembershipCommand command = ModifyMembershipCommand.builder()
+        assertThrows(ValidationException.class, () -> {
+            ModifyMembershipCommand command = builder()
                     .membershipId("1")
                     .name("hi")
                     .address("email")
@@ -25,8 +27,9 @@ class ModifyMembershipCommandTest {
     }
 
     @Test
+    @DisplayName("ModifyMembershipCommand 성공 테스트")
     void validateSuccessSelf() {
-        assertThat(ModifyMembershipCommand.builder()
+        assertThat(builder()
                 .membershipId("1")
                 .name("hi")
                 .address("email")
